03785539a847892d80520408ab7dfacd81af06e9
Miguel Ruiz <miguel.ruiz.dev@gmail.com>
2021-10-22 14:12:23 +0100

    Fix APA query client generation via patch script

--- alfresco-apa-java-rest-api/alfresco-apa-java-rest-api-lib/generated/alfresco-activiti-audit-rest-api/src/main/java/com/alfresco/activiti/audit/handler/ApplicationAdminControllerApiClient.java
+++ alfresco-apa-java-rest-api/alfresco-apa-java-rest-api-lib/generated/alfresco-activiti-audit-rest-api/src/main/java/com/alfresco/activiti/audit/handler/ApplicationAdminControllerApiClient.java
@@ -18,6 +18,6 @@ package com.alfresco.activiti.audit.handler;
 import org.springframework.cloud.openfeign.FeignClient;
 import io.swagger.configuration.ClientConfiguration;
 
-@FeignClient(value = "${alfrescoProcessQueryServiceReST_.name:alfrescoProcessQueryServiceReST_}", url = "${alfrescoProcessQueryServiceReST_.url:}", configuration = ClientConfiguration.class, decode404 = true)
+@FeignClient(value = "query", url = "${activiti.service.query.url}", path = "${activiti.service.query.path}", configuration = ClientConfiguration.class, decode404 = true)
 public interface ApplicationAdminControllerApiClient extends ApplicationAdminControllerApi {
 }
