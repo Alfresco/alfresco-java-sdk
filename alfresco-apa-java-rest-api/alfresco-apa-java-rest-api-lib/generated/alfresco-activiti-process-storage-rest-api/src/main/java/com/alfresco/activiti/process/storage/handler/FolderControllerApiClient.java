3c9f88c9765cdd0c7dceff793be1beab796dc298
Miguel Ruiz <miguel.ruiz.dev@gmail.com>
2021-10-22 15:23:35 +0100

    Generation fix

--- alfresco-apa-java-rest-api/alfresco-apa-java-rest-api-lib/generated/alfresco-activiti-process-storage-rest-api/src/main/java/com/alfresco/activiti/process/storage/handler/FolderControllerApiClient.java
+++ alfresco-apa-java-rest-api/alfresco-apa-java-rest-api-lib/generated/alfresco-activiti-process-storage-rest-api/src/main/java/com/alfresco/activiti/process/storage/handler/FolderControllerApiClient.java
@@ -18,6 +18,6 @@ package com.alfresco.activiti.process.storage.handler;
 import org.springframework.cloud.openfeign.FeignClient;
 import io.swagger.configuration.ClientConfiguration;
 
-@FeignClient(value = "${alfrescoProcessStorageServiceReST_.name:alfrescoProcessStorageServiceReST_}", url = "${alfrescoProcessStorageServiceReST_.url:}", configuration = ClientConfiguration.class, decode404 = true)
+@FeignClient(value = "process-storage", url = "${activiti.service.process.storage.url}", path = "${activiti.service.process.storage.path}", configuration = ClientConfiguration.class, decode404 = true)
 public interface FolderControllerApiClient extends FolderControllerApi {
 }
