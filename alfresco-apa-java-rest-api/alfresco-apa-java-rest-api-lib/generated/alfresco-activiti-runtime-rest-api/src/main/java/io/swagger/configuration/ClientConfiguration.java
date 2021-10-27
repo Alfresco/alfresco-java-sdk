39fb7e5cd17516a4b30dfa3c3ad7e186d35b4110
Miguel Ruiz <miguel.ruiz.dev@gmail.com>
2021-10-27 16:27:55 +0100

    Regeneate APA clients

--- alfresco-apa-java-rest-api/alfresco-apa-java-rest-api-lib/generated/alfresco-activiti-runtime-rest-api/src/main/java/io/swagger/configuration/ClientConfiguration.java
+++ alfresco-apa-java-rest-api/alfresco-apa-java-rest-api-lib/generated/alfresco-activiti-runtime-rest-api/src/main/java/io/swagger/configuration/ClientConfiguration.java
@@ -15,17 +15,23 @@
  */
 package io.swagger.configuration;
 
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
+import org.alfresco.rest.sdk.feign.config.BasicAuthConfiguration;
+import org.alfresco.rest.sdk.feign.config.DelegatedAuthenticationConfiguration;
+import org.alfresco.rest.sdk.feign.config.OAuth2Configuration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
-
+/**
+* Feign client Spring configuration that provides support for different authentication methods. Currently, supported methods are:
+* <ul>
+  *     <li>Basic Authentication</li>
+  *     <li>OAuth2 - Client credentials flow</li>
+  *     <li>OAuth2 - Password flow</li>
+  *     <li>Delegated (external) authentication</li>
+  * </ul>
+*/
 @Configuration
-@EnableConfigurationProperties
+@Import({BasicAuthConfiguration.class, OAuth2Configuration.class, DelegatedAuthenticationConfiguration.class})
 public class ClientConfiguration {
 
 }
