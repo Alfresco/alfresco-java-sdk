02aeaac37c4c22607f20924fceb3a03ac1c26dd6
Miguel Ruiz <miguel.ruiz.dev@gmail.com>
2021-10-29 15:21:54 +0100

    Fetch task action

--- samples/java-rest-api-clients/src/main/java/org/alfresco/java/rest/client/sample/service/RESTClientService.java
+++ samples/java-rest-api-clients/src/main/java/org/alfresco/java/rest/client/sample/service/RESTClientService.java
@@ -16,11 +16,18 @@
 package org.alfresco.java.rest.client.sample.service;
 
 import com.alfresco.activiti.runtime.handler.ProcessInstanceControllerImplApiClient;
+import com.alfresco.activiti.runtime.handler.ProcessInstanceTasksControllerImplApi;
+import com.alfresco.activiti.runtime.handler.TaskControllerImplApi;
+import com.alfresco.activiti.runtime.model.EntryResponseContentOfCloudProcessInstance;
+import com.alfresco.activiti.runtime.model.EntryResponseContentOfCloudTask;
+import com.alfresco.activiti.runtime.model.ListResponseContentOfCloudTask;
 import com.alfresco.activiti.runtime.model.StartProcessPayload;
-import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -28,34 +35,58 @@ import org.springframework.stereotype.Component;
 public class RESTClientService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RESTClientService.class);
+    public static final String PROCESS_DEFINITION_KEY = "SingleTaskProcess";
 
     private final ProcessInstanceControllerImplApiClient processInstanceControllerImplApiClient;
 
-    public RESTClientService(ProcessInstanceControllerImplApiClient processInstanceControllerImplApiClient) {
+    private final ProcessInstanceTasksControllerImplApi processInstanceTasksControllerImplApi;
+
+    public RESTClientService(ProcessInstanceControllerImplApiClient processInstanceControllerImplApiClient,
+        ProcessInstanceTasksControllerImplApi processInstanceTasksControllerImplApi) {
         this.processInstanceControllerImplApiClient = processInstanceControllerImplApiClient;
+        this.processInstanceTasksControllerImplApi = processInstanceTasksControllerImplApi;
     }
 
     @Scheduled(fixedRate = 5000)
     public void startProcessInstance(){
 
-        LOGGER.info("STARTING PROCESS");
+        LOGGER.info(String.format("Starting new process instance with %s definition key", PROCESS_DEFINITION_KEY));
 
-        processInstanceControllerImplApiClient.startProcessUsingPOST1(buildStartProcessPayload(
-            "SingleTaskProcess",
-            null
-        ));
+        ResponseEntity<EntryResponseContentOfCloudProcessInstance> responseEntity = processInstanceControllerImplApiClient
+            .startProcessUsingPOST1(buildStartProcessPayload(PROCESS_DEFINITION_KEY));
 
-        LOGGER.info("PROCESS STARTED");
+        String processId = responseEntity.getBody().getEntry().getId();
+
+        LOGGER.info(String.format("Process instance with id: %s created!", processId));
+
+        LOGGER.info("Fetching task of the created process instance");
+
+        ResponseEntity<ListResponseContentOfCloudTask> tasksOfProcessInstance = processInstanceTasksControllerImplApi
+            .getTasksUsingGET(processId, null, null, null);
+
+        Optional<EntryResponseContentOfCloudTask> optionalTask = tasksOfProcessInstance
+            .getBody()
+            .getList()
+            .getEntries()
+            .stream()
+            .findFirst();
+
+        String taskId = optionalTask
+            .get()
+            .getEntry()
+            .getId();
+
+        LOGGER.info(String.format("Task of process instance with id: %s fetched!", taskId));
+
+
+
     }
 
-    private StartProcessPayload buildStartProcessPayload(String processDefinitionKey,
-        Map<String, Object> variables) {
+    private StartProcessPayload buildStartProcessPayload(String processDefinitionKey) {
         StartProcessPayload startProcessPayload = new StartProcessPayload();
         startProcessPayload.setId(UUID.randomUUID().toString());
         startProcessPayload.setPayloadType(StartProcessPayload.PayloadTypeEnum.STARTPROCESSPAYLOAD);
-//        startProcessPayload.setProcessDefinitionId(processDefinitionId);
         startProcessPayload.setProcessDefinitionKey(processDefinitionKey);
-        startProcessPayload.setVariables(variables);
         return startProcessPayload;
     }
 }
