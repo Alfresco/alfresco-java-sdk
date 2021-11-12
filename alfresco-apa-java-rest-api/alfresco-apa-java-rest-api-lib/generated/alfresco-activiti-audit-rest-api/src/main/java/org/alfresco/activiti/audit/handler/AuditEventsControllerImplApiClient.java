59bd6cc55cb69c0f1e861145196dc8b3830c1c7b
Miguel Ruiz <miguel.ruiz.dev@gmail.com>
2021-11-12 13:28:24 +0000

    Regenerate audit clients

--- alfresco-apa-java-rest-api/alfresco-apa-java-rest-api-lib/generated/alfresco-activiti-audit-rest-api/src/main/java/org/alfresco/activiti/audit/handler/AuditEventsControllerImplApiClient.java
+++ alfresco-apa-java-rest-api/alfresco-apa-java-rest-api-lib/generated/alfresco-activiti-audit-rest-api/src/main/java/org/alfresco/activiti/audit/handler/AuditEventsControllerImplApiClient.java
@@ -18,6 +18,6 @@ package org.alfresco.activiti.audit.handler;
 import org.springframework.cloud.openfeign.FeignClient;
 import io.swagger.configuration.ClientConfiguration;
 
-@FeignClient(value = "alfrescoAuditEventsControllerImplApi", url = "${auditServiceReST_.url:}", configuration = ClientConfiguration.class)
+@FeignClient(value = "alfrescoAuditEventsControllerImplApi", url = "${activiti.service.audit.url}", path = "${activiti.service.audit.path}", configuration = ClientConfiguration.class)
 public interface AuditEventsControllerImplApiClient extends AuditEventsControllerImplApi {
 }
--- alfresco-apa-java-rest-api/alfresco-apa-java-rest-api-lib/generated/alfresco-activiti-audit-rest-api/src/main/java/org/alfresco/activiti/audit/handler/AuditEventsDeleteControllerApiClient.java
+++ alfresco-apa-java-rest-api/alfresco-apa-java-rest-api-lib/generated/alfresco-activiti-audit-rest-api/src/main/java/org/alfresco/activiti/audit/handler/AuditEventsDeleteControllerApiClient.java
@@ -18,6 +18,6 @@ package org.alfresco.activiti.audit.handler;
 import org.springframework.cloud.openfeign.FeignClient;
 import io.swagger.configuration.ClientConfiguration;
 
-@FeignClient(value = "alfrescoAuditEventsDeleteControllerApi", url = "${auditServiceReST_.url:}", configuration = ClientConfiguration.class)
+@FeignClient(value = "alfrescoAuditEventsDeleteControllerApi", url = "${activiti.service.audit.url}", path = "${activiti.service.audit.path}", configuration = ClientConfiguration.class)
 public interface AuditEventsDeleteControllerApiClient extends AuditEventsDeleteControllerApi {
 }
