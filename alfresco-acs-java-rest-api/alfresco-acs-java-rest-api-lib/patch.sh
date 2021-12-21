90125485bc0dac201e5bb181e0bc3fd2156fed73
dcerbo <davide.cerbo@alfresco.com>
2021-12-21 11:52:14 +0100

    AAE-6349 change script to make it wokring with MacOS and Linux

--- alfresco-acs-java-rest-api/alfresco-acs-java-rest-api-lib/patch.sh
+++ alfresco-acs-java-rest-api/alfresco-acs-java-rest-api-lib/patch.sh
@@ -2,6 +2,14 @@
 set -e
 
 GENERATED_SOURCE_DIR=${GENERATED_SOURCE_DIR:-generated}
+case $(uname | tr '[:upper:]' '[:lower:]') in
+  darwin*)
+    export EDIT_FILE_IN_PLACE_PARAM="''"
+    ;;
+  *)
+    export EDIT_FILE_IN_PLACE_PARAM=
+    ;;
+esac
 
 find "${GENERATED_SOURCE_DIR}" -type f -name "*.java" -exec sed \
   -e 's;, url = "${processServicesEnterprise_.url:.*}";, url = "${process.service.url}", path = "${process.service.path}";g' \
@@ -22,7 +30,7 @@ find "${GENERATED_SOURCE_DIR}" -type f -name "*.java" -exec sed \
   -e 's;"${alfrescoInsightEngineREST_.security.basicAuth.password:.*}";"${content.service.security.basicAuth.password}";g' \
   -e 's;"alfrescoInsightEngineREST_.security.basicAuth.username";"content.service.security.basicAuth.username";g' \
   -e 's@import com\.alfresco\..*\.ResponseEntity;@@g' \
-  -i {} +
+  -i $EDIT_FILE_IN_PLACE_PARAM {} +
 
 find "${GENERATED_SOURCE_DIR}" -type f -name "*.md" -exec sed \
   -e 's, *ApiClient defaultClient = Configuration.getDefaultApiClient();,,g' \
@@ -30,7 +38,7 @@ find "${GENERATED_SOURCE_DIR}" -type f -name "*.md" -exec sed \
   -e 's, *HttpBasicAuth basicAuth = (HttpBasicAuth) defaultClient.getAuthentication("basicAuth");,,g' \
   -e 's, *basicAuth.setUsername("YOUR USERNAME");,,g' \
   -e 's, *basicAuth.setPassword("YOUR PASSWORD");,,g' \
-  -i {} +
+  -i $EDIT_FILE_IN_PLACE_PARAM {} +
 
 find "${GENERATED_SOURCE_DIR}" -type f -name 'NodeBody*.java' -exec sed \
   -e 's;private Map<String, String> properties = null;private Map<String, Object> properties = null;g' \
@@ -39,7 +47,7 @@ find "${GENERATED_SOURCE_DIR}" -type f -name 'NodeBody*.java' -exec sed \
   -e 's;public void setProperties(Map<String, String> properties);public void setProperties(Map<String, Object> properties);g' \
   -e 's;this.properties = new HashMap<String, String>();this.properties = new HashMap<String, Object>();g' \
   -e 's;putPropertiesItem(String key, String propertiesItem);putPropertiesItem(String key, Object propertiesItem);g' \
-  -i  {} +
+  -i $EDIT_FILE_IN_PLACE_PARAM {} +
 
 
 find ${GENERATED_SOURCE_DIR} -type f -iname 'ModelsApi.java' -exec sed \
@@ -47,15 +55,15 @@ find ${GENERATED_SOURCE_DIR} -type f -iname 'ModelsApi.java' -exec sed \
   -e 's/ResponseEntity<Void> exportModelUsingGET/byte[] exportModelUsingGET/' \
   -e 's/ResponseEntity<String>/byte[]/' \
   -e 's/@RequestParam("file")/@PathVariable("file")/' \
-  -i  {} +
+  -i $EDIT_FILE_IN_PLACE_PARAM {} +
 
 find ${GENERATED_SOURCE_DIR} -type f -name 'SearchApiClient.java' -exec sed \
   -e 's/path = "${content.service.path}"/path = "${search.service.path}"/' \
-  -i  {} +
+  -i $EDIT_FILE_IN_PLACE_PARAM {} +
 
 find ${GENERATED_SOURCE_DIR} -type f -name 'DiscoveryApiClient.java' -exec sed \
   -e 's/path = "${content.service.path}"/path = "${discovery.service.path}"/' \
-  -i  {} +
+  -i $EDIT_FILE_IN_PLACE_PARAM {} +
 
 find "${GENERATED_SOURCE_DIR}" -type d \( \
   -name 'gradle' \
@@ -65,7 +73,7 @@ find "${GENERATED_SOURCE_DIR}" -type d \( \
 
 find "${GENERATED_SOURCE_DIR}" -type f -iname 'README.md' -exec sed \
   -e '/ReleasesApi/d' \
-  -i  {} +
+  -i $EDIT_FILE_IN_PLACE_PARAM {} +
 
 find "${GENERATED_SOURCE_DIR}" -type f \( \
   -name 'ResponseEntity*.java' \
