0ef7dbcf5df30bffeb97394b2504a8461c445cf8
omkar.yadav <omkar.yadav@globallogic.com>
2024-08-12 17:43:44 +0530

    Added release Job in GitHub Action

--- scripts/ci/maven_release.sh
+++ scripts/ci/maven_release.sh
@@ -3,7 +3,7 @@
 echo "=========================== Starting Release Script ==========================="
 PS4="\[\e[35m\]+ \[\e[m\]"
 set -vex
-pushd "$(dirname "${BASH_SOURCE[0]}")/../"
+pushd "$(dirname "${BASH_SOURCE[0]}")/../../"
 
 # Github Actions CI runner work on DETACHED HEAD, so we need to checkout the release branch
 git checkout -B "${BRANCH_NAME}"
@@ -16,11 +16,11 @@ git config user.name "${GIT_USERNAME}"
 
 # Run the release plugin - with "[skip ci]" in the release commit message
 mvn -B \
-  "-Darguments=-DskipTests -Dbuild-number=${BUILD_NUMBER}" \
-  release:clean release:prepare release:perform \
-  -DscmCommentPrefix="[maven-release-plugin][skip ci] " \
-  -Dusername="${GIT_USERNAME}" \
-  -Dpassword="${GIT_PASSWORD}"
+    "-Darguments=-DskipTests -DbuildNumber=$GITHUB_RUN_NUMBER" \
+    release:clean release:prepare release:perform \
+    -DscmCommentPrefix="[maven-release-plugin][skip ci] " \
+    -Dusername="${GIT_USERNAME}" \
+    -Dpassword="${GIT_PASSWORD}"
 
 popd
 set +vex
