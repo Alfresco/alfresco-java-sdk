5c4342efa57ca223f238ea2249794883bb72a075
omkar.yadav <omkar.yadav@globallogic.com>
2024-08-12 17:19:00 +0530

    Added release Job in GitHub Action

---
+++ scripts/ci/maven_release.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+echo "=========================== Starting Release Script ==========================="
+PS4="\[\e[35m\]+ \[\e[m\]"
+set -vex
+pushd "$(dirname "${BASH_SOURCE[0]}")/../"
+
+# Github Actions CI runner work on DETACHED HEAD, so we need to checkout the release branch
+git checkout -B "${BRANCH_NAME}"
+
+git pull
+
+# Add email to link commits to user
+git config user.email "${GIT_EMAIL}"
+git config user.name "${GIT_USERNAME}"
+
+# Run the release plugin - with "[skip ci]" in the release commit message
+mvn -B \
+    "-Darguments=-DskipTests -DbuildNumber=$GITHUB_RUN_NUMBER" \
+    release:clean release:prepare release:perform \
+    -DscmCommentPrefix="[maven-release-plugin][skip ci] " \
+    -Dusername="${GIT_USERNAME}" \
+    -Dpassword="${GIT_PASSWORD}"
+
+popd
+set +vex
+echo "=========================== Finishing Release Script =========================="
