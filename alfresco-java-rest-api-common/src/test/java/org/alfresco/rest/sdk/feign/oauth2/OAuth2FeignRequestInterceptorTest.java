661e1ff9d0e6a74e970113ac93ed368edf861d6d
pmm <purusothaman.mm@hyland.com>
2024-12-05 17:38:10 +0530

    [MNT-24565] removed header changes

--- alfresco-java-rest-api-common/src/test/java/org/alfresco/rest/sdk/feign/oauth2/OAuth2FeignRequestInterceptorTest.java
+++ alfresco-java-rest-api-common/src/test/java/org/alfresco/rest/sdk/feign/oauth2/OAuth2FeignRequestInterceptorTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021-2024 Alfresco Software, Ltd.
+ * Copyright 2021-2021 Alfresco Software, Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
