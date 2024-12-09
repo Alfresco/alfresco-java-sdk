8112c1c67ed1a812043ed8fa02041d2e44f1c7fa
pmm <purusothaman.mm@hyland.com>
2024-12-09 11:10:36 +0530

    [MNT-24565] updated formatting issues

--- alfresco-java-rest-api-common/src/main/java/org/alfresco/rest/sdk/feign/oauth2/OAuth2FeignRequestInterceptor.java
+++ alfresco-java-rest-api-common/src/main/java/org/alfresco/rest/sdk/feign/oauth2/OAuth2FeignRequestInterceptor.java
@@ -37,7 +37,6 @@ import feign.RequestTemplate;
  */
 public class OAuth2FeignRequestInterceptor implements RequestInterceptor
 {
-
     private static final Logger LOGGER = LoggerFactory.getLogger(OAuth2FeignRequestInterceptor.class);
     private static final String AUTH_HEADER_FORMAT = "%s %s";
     private static final String REFRESH_TOKEN_ATTRIBUTE_NAME = "refresh_token";
