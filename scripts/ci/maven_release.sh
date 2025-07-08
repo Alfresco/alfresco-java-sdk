ec8f120339349acdc2ae9a3af406f0e6963628f2
tathagta15 <Tathagata.Nandi@hyland.com>
2025-07-08 09:47:57 +0000

    changed release script

--- scripts/ci/maven_release.sh
+++ scripts/ci/maven_release.sh
@@ -10,16 +10,12 @@ git checkout -B "${BRANCH_NAME}"
 
 git pull
 
-# Add email to link commits to user
-git config user.email "${GIT_EMAIL}"
-git config user.name "${GIT_USERNAME}"
-
 # Run the release plugin - with "[skip ci]" in the release commit message
 mvn -B \
     "-Darguments=-DskipTests -DbuildNumber=$GITHUB_RUN_NUMBER" \
     release:clean release:prepare release:perform \
     -DscmCommentPrefix="[maven-release-plugin][skip ci] " \
-    -Dusername="${GIT_USERNAME}" \
+    -Dusername=alfresco-build \
     -Dpassword="${GIT_PASSWORD}"
 
 popd
