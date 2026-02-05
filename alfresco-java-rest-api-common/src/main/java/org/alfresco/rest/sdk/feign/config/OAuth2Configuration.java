1f2e4cfda1a02a325279e37b16640d0ff7274fb2
Kacper Magdziarz <kacper.magdziarz@hyland.com>
2026-02-05 12:27:18 +0100

    [ACS-11053] Bump Spring Boot to 4

--- alfresco-java-rest-api-common/src/main/java/org/alfresco/rest/sdk/feign/config/OAuth2Configuration.java
+++ alfresco-java-rest-api-common/src/main/java/org/alfresco/rest/sdk/feign/config/OAuth2Configuration.java
@@ -20,16 +20,15 @@ import org.alfresco.rest.sdk.feign.oauth2.OAuth2FeignRequestInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
-import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientPropertiesMapper;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.security.oauth2.client.autoconfigure.OAuth2ClientProperties;
+import org.springframework.boot.security.oauth2.client.autoconfigure.OAuth2ClientPropertiesMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.OAuth2AuthorizationContext;
 import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
@@ -75,7 +74,6 @@ public class OAuth2Configuration {
     public OAuth2AuthorizedClientProvider oAuth2AuthorizedClientProvider() {
         return OAuth2AuthorizedClientProviderBuilder.builder()
             .clientCredentials()
-            .password()
             .refreshToken()
             .build();
     }
@@ -95,8 +93,6 @@ public class OAuth2Configuration {
         return OAuth2AuthorizeRequest.withClientRegistrationId(OAUTH2_CLIENT_REGISTRATION_ID)
             .principal(new AnonymousAuthenticationToken("feignClient", "feignClient",
                 AuthorityUtils.createAuthorityList("ROLE_ANONYMOUS")))
-            .attribute(OAuth2AuthorizationContext.USERNAME_ATTRIBUTE_NAME, oAuth2Username)
-            .attribute(OAuth2AuthorizationContext.PASSWORD_ATTRIBUTE_NAME, oAuth2Password)
             .build();
     }
 
--- samples/extension-template/src/main/java/org/alfresco/sdk/sample/ExtensionTemplateApplication.java
+++ samples/extension-template/src/main/java/org/alfresco/sdk/sample/ExtensionTemplateApplication.java
@@ -18,7 +18,7 @@ package org.alfresco.sdk.sample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={SecurityAutoConfiguration.class})
